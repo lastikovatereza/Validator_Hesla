d8fb859e54f08254ee1b0f7bebb89dfadfccbc3a
Tereza <lastikova.tereza@gmail.com>
2025-08-17 17:24:47 +0200

    Opravena chyba + přidány poznámky

--- validator_hesla.py
+++ validator_hesla.py
@@ -7,9 +7,7 @@ class ValidatorHesla:
         for char in heslo:
             if '0' <= char <= '9':      # musí obsahovat aspoň jednu číslici
                 return True
-            return False
-
-
+        return False                    # return musí byt mimo loop
 
         # Příklad použití:
 
@@ -18,5 +16,16 @@ if __name__ == "__main__":
     print(ValidatorHesla.je_validni("abcdef"))   # False (bez čísla)
     print(ValidatorHesla.je_validni("abc123"))   # True
     print(ValidatorHesla.je_validni("123456"))   # True
+
+
+
+# nebo bez statické metody
+"""    def validuj(self, heslo):
+        if len(heslo) <= 5:
+            return False
+        for char in heslo:
+            if '0' <= char <= '9': 
+                return True
+            return False """
 
 
